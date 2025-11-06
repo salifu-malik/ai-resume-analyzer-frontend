051f8fa05479e48fcd8aa601feee58d3d0aee85b
salifu-malik <salifuabdulmalik69@gmail.com>
2025-11-06 11:59:51 +0000

    Update To repos

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,52 +1,51 @@
-import React, {useEffect, useState} from 'react'
 import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
 import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
-    {title: 'Resucheck | Auth'},
-    {name: 'description', content: 'Log into your account'},
+    { title: 'Resucheck | Auth' },
+    { name: 'description', content: 'Log into your account' },
 ])
 
 const Auth = () => {
-const {isLoading, auth} = usePuterStore();
-const location = useLocation();
-const next = location.search.split("next=")[1];
-const navigate = useNavigate();
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
 
-useEffect(() => {
-if(auth.isAuthenticated) navigate(next);
-}, [auth.isAuthenticated, next]);
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next])
+
     return (
-        <main className = "bg-[url(images/bg-main.svg)] bg-cover main-screen flex item-center justify-center">
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
-                <section className="flex flex-col gab-8 bg-white rounded-2xl p-10">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
                     <div className="flex flex-col items-center gap-2 text-center">
                         <h1>Welcome</h1>
-                        <h2>Log In To Continue Your Job Search Path</h2>
+                        <h2>Log In to Continue Your Job Journey</h2>
                     </div>
-                    <div className="flex flex-col items-center gap-2 text-center">
-                        {isLoading}(
-                        <button className="auth-button animate-pulse">
-                            <p> Signing you in...</p>
-                        </button>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in...</p>
+                            </button>
                         ) : (
                             <>
-                    {auth.isAuthenticated ? (
-                    <button className="auth-button" onClick={auth.signOut}>
-                        <p>Log Out</p>
-                    </button>
-                    ) : (
-                        <button className="auth-button" onClick={auth.signIn}>
-                            <p>Log In</p>
-                        </button>
-
-                    )}
-                        </>
+                                {auth.isAuthenticated ? (
+                                    <button className="auth-button" onClick={auth.signOut}>
+                                        <p>Log Out</p>
+                                    </button>
+                                ) : (
+                                    <button className="auth-button" onClick={auth.signIn}>
+                                        <p>Log In</p>
+                                    </button>
+                                )}
+                            </>
                         )}
                     </div>
                 </section>
             </div>
-
         </main>
     )
 }
