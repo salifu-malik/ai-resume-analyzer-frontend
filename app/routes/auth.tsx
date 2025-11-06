8a3de48a05bf8a5f380dbe5e722de4b471352045
salifu-malik <salifuabdulmalik69@gmail.com>
2025-11-06 10:17:54 +0000

    Update To repos

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,4 +1,6 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
     {title: 'Resucheck | Auth'},
@@ -6,7 +8,14 @@ export const meta = () => ([
 ])
 
 const Auth = () => {
+const {isLoading, auth} = usePuterStore();
+const location = useLocation();
+const next = location.search.split("next=")[1];
+const navigate = useNavigate();
 
+useEffect(() => {
+if(auth.isAuthenticated) navigate(next);
+}, [auth.isAuthenticated, next]);
     return (
         <main className = "bg-[url(images/bg-main.svg)] bg-cover main-screen flex item-center justify-center">
             <div className="gradient-border shadow-lg">
@@ -14,6 +23,26 @@ const Auth = () => {
                     <div className="flex flex-col items-center gap-2 text-center">
                         <h1>Welcome</h1>
                         <h2>Log In To Continue Your Job Search Path</h2>
+                    </div>
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        {isLoading}(
+                        <button className="auth-button animate-pulse">
+                            <p> Signing you in...</p>
+                        </button>
+                        ) : (
+                            <>
+                    {auth.isAuthenticated ? (
+                    <button className="auth-button" onClick={auth.signOut}>
+                        <p>Log Out</p>
+                    </button>
+                    ) : (
+                        <button className="auth-button" onClick={auth.signIn}>
+                            <p>Log In</p>
+                        </button>
+
+                    )}
+                        </>
+                        )}
                     </div>
                 </section>
             </div>
